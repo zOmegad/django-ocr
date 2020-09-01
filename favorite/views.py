b90d3e5b048667ef01aea0fb618492f48d1fef5c
zOmegad <omegad@protonmail.com>
2020-09-01 12:50:32 +0200

    favorite - front end

--- favorite/views.py
+++ favorite/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from favorite.models import SavedProduct
 
 # Create your views here.
 
 def my_save_food(request):
 	my_food = SavedProduct.objects.filter(user=request.user)
+	paginator = Paginator(my_food, 6)
 
-	return render(request, 'my_save_food.html', {'my_food': my_food})
+	page_number = request.GET.get('page')
+	page_obj = paginator.get_page(page_number)
+	return render(request, 'my_save_food.html', {'my_food': my_food, 'page_obj': page_obj})
