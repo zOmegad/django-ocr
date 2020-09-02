1838e1cf8cdba40954aec0647bed8a816f3b8f31
zOmegad <omegad@protonmail.com>
2020-09-02 17:03:46 +0200

    add first tests

--- food/tests.py
+++ food/tests.py
@@ -1,3 +1,16 @@
+from django.urls import reverse
 from django.test import TestCase
 
-# Create your tests here.
+from food.models import Product
+
+class IndexPageTestCase(TestCase):
+    def test_index_page_returns_200(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+
+class ShowPageTestCase(TestCase):
+    def test_show_with_product_returns_200(self):
+        product = Product.objects.create(name="Pasta", nutriscore=1,)
+        food = Product.objects.get(pk=1)
+        response = self.client.get(reverse('show', args=[food.id]))
+        self.assertEqual(response.status_code, 200)
--- food/urls.py
+++ food/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 
 urlpatterns = [
-	path('', views.index),
+	path('', views.index, name='index'),
 	path('search/', views.search, name='search'),
     path('<int:food_id>/', views.show, name='show'),
     path('<int:food_id>/save_product', views.save_product, name='save_product'),
