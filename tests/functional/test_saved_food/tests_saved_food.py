a1ed449812dc15784580f716cb1cff392f196595
Debian <debian@vps-c44f347e.vps.ovh.net>
2021-05-31 13:15:09 +0000

    edit link

--- tests/functional/test_saved_food/tests_saved_food.py
+++ tests/functional/test_saved_food/tests_saved_food.py
@@ -10,7 +10,7 @@ class SavedProductTest(LiveServerTestCase):
         selenium = webdriver.Firefox(
             executable_path=r'/home/travis/build/'
             'zOmegad/django-ocr/geckodriver', firefox_options=opts)
-        selenium.get('http://127.0.0.1:8000/accounts/login/')
+        selenium.get('http:///accounts/login/')
         username = selenium.find_element_by_id("username_field")
         password = selenium.find_element_by_id("password_field")
 
@@ -18,10 +18,10 @@ class SavedProductTest(LiveServerTestCase):
         password.send_keys("lol")
         selenium.find_element_by_id("submit_btn").click()
 
-        selenium.get('http://127.0.0.1:8000/search/?query=Jambon')
+        selenium.get('http://141.94.70.168/search/?query=Jambon')
         save_button = selenium.find_element_by_id('save_btn_59201')
         save_button.click()
-        selenium.get('http://127.0.0.1:8000/favorite/my_save_food/')
+        selenium.get('http://141.94.70.168/favorite/my_save_food/')
         food_card = selenium.find_element_by_class_name("card-title")
         self.assertEqual(food_card.text, "Pisellini Primavera")
         selenium.quit()
