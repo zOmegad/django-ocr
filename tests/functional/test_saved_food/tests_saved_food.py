3463c0b8de1074d7a83ad84b175d1c6bfc06589e
zOmegad <omegad@protonmail.com>
2021-05-31 18:26:04 +0200

    source page print

--- tests/functional/test_saved_food/tests_saved_food.py
+++ tests/functional/test_saved_food/tests_saved_food.py
@@ -22,7 +22,7 @@ class SavedProductTest(LiveServerTestCase):
         selenium.get('http://141.94.70.168/search/?query=Haricot')
         selenium.implicitly_wait(5)
         source_page = selenium.page_source
-        print(page_source)
+        print(source_page)
         selenium.find_element_by_id("save_btn_21").click()
         selenium.get('http://141.94.70.168/favorite/my_save_food/')
         food_card = selenium.find_element_by_class_name("card-title")
