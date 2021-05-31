60dc39ca64bd47e33a7b2250ac0137a7dbc8c7da
zOmegad <omegad@protonmail.com>
2021-05-31 18:19:00 +0200

    savebtn

--- tests/functional/test_saved_food/tests_saved_food.py
+++ tests/functional/test_saved_food/tests_saved_food.py
@@ -15,13 +15,13 @@ class SavedProductTest(LiveServerTestCase):
         username = selenium.find_element_by_id("username_field")
         password = selenium.find_element_by_id("password_field")
 
-        username.send_keys("lol")
+        username.send_keys("user")
         password.send_keys("azerty12345")
         selenium.find_element_by_id("submit_btn").click()
 
         selenium.get('http://141.94.70.168/search/?query=Haricot')
-        selenium.implicitly_wait(10)
-        selenium.find_element_by_xpath('//*[@id="save_btn_21"]').click()
+        selenium.implicitly_wait(5)
+        selenium.find_element_by_id("save_btn_21").click()
         selenium.get('http://141.94.70.168/favorite/my_save_food/')
         food_card = selenium.find_element_by_class_name("card-title")
         self.assertEqual(food_card.text, "Fonio complet")
