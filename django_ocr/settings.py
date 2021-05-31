c2d54bee54cb8e7f57fda57c3ce2ff7e24524912
zOmegad <omegad@protonmail.com>
2021-05-31 20:14:35 +0200

    travis settings for db

--- django_ocr/settings.py
+++ django_ocr/settings.py
@@ -84,16 +84,29 @@ WSGI_APPLICATION = 'django_ocr.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': "django.db.backends.postgresql_psycopg2",
-        'NAME': "ocr_petitbeurre",
-        'USER': "postgres",
-        'PASSWORD': "",
-        'HOST': 'localhost',
-        'PORT': "5432",
+if os.getenv('BUILD_ON_TRAVIS', None):
+    DATABASES = {
+        'default': {
+            'ENGINE': "django.db.backends.postgresql_psycopg2",
+            'NAME': "ocr_petitbeurre",
+            'USER': "postgres",
+            'PASSWORD': "",
+            'HOST': 'localhost',
+            'PORT': "5432",
+        }
     }
-}
+    
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'ocr_petitbeurre',
+            'USER': 'johnson',
+            'PASSWORD': os.getenv("DATABASE_PASSWORD"),
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
 
 
 # Password validation
